85ce5b55c32b48d75a8299cd99a631e8c66f4a80
MihailCorcimari <mhcorcimari@gmail.com>
2025-07-11 21:26:18 +0100

    Create database and countries table

---
+++ SalesCet95.Web/Data/Entities/Country.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Cors;
+using System.ComponentModel.DataAnnotations;
+
+namespace SalesCet95.Web.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name="Pais")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no maximo {1} caracteres!")]
+        [Required(ErrorMessage ="O campo {0} é obrigatório!")]
+        public string Name { get; set; }
+        
+    }
+}
